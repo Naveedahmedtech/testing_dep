e3b3c726e3367187ee335679cab912f01211f836
Naveedahmedtech <technaveedahmed@gmail.com>
2024-11-12 01:03:58 +0500

    update cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express from "express";
 import logger from "./config/logger";
 import { PORT, NODE_ENV } from "./config";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
+
 app.use("/api", routes);
 
 app.listen(PORT, () => {

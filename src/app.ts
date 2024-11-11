8caf589c8fc39e75a425fda00c485edf6ffbfaaa
Naveedahmedtech <technaveedahmed@gmail.com>
2024-11-12 01:11:34 +0500

    session middleware

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,21 @@ import logger from "./config/logger";
 import { PORT, NODE_ENV } from "./config";
 import routes from "./routes";
 import cors from "cors";
+import session from "express-session";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
+  app.use(
+    session({
+      secret: "session-secret-key",
+      resave: false,
+      saveUninitialized: true,
+      cookie: { secure: false },
+    })
+  );
 app.use("/api", routes);
 
 app.listen(PORT, () => {
